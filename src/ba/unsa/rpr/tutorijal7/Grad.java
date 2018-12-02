0c66b5697522e2d9406eac497f0309631c435fc3
shrbenic1 <shrbenic1@etf.unsa.ba>
2018-12-02 15:07:04 +0100

    Kreiran konstruktor bez parametara, setteri i getteri

--- src/ba/unsa/rpr/tutorijal7/Grad.java
+++ src/ba/unsa/rpr/tutorijal7/Grad.java
@@ -4,4 +4,31 @@ public class Grad {
     private String naziv;
     private int brojStanovnika;
     private double[] temperature = new double[1000];
+
+    public Grad() {
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public int getBrojStanovnika() {
+        return brojStanovnika;
+    }
+
+    public double[] getTemperature() {
+        return temperature;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public void setBrojStanovnika(int brojStanovnika) {
+        this.brojStanovnika = brojStanovnika;
+    }
+
+    public void setTemperature(double[] temperature) {
+        this.temperature = temperature;
+    }
 }
