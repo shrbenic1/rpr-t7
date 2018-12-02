d165b6b0d5ccf8115a2810e4273974908bf84929
shrbenic1 <shrbenic1@etf.unsa.ba>
2018-12-02 17:56:31 +0100

    Implementirana metoda ucitajGradove

--- src/ba/unsa/rpr/tutorijal7/Tutorijal.java
+++ src/ba/unsa/rpr/tutorijal7/Tutorijal.java
@@ -1,7 +1,48 @@
 package ba.unsa.rpr.tutorijal7;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Tutorijal {
     public static void main(String[] args) {
+        Tutorijal.ucitajGradove();
+    }
 
+    public static ArrayList<Grad> ucitajGradove() {
+        ArrayList<Grad> gradovi = new ArrayList<Grad>();
+        Scanner ulaz;
+        try {
+            ulaz = new Scanner(new FileReader("mjerenja.txt"));
+        } catch(FileNotFoundException e) {
+            System.out.println("Datoteka mjerenja.txt se ne može otvoriti");
+            return gradovi;
+        }
+        try {
+            while(ulaz.hasNext()) {
+                String[] podaci = ulaz.nextLine().split(",");
+                Grad grad = new Grad();
+                grad.setNaziv(podaci[0]);
+                double[] niz = new double[podaci.length - 1];
+                for(int i = 1; i < 1000; i++) {
+                    if(i == podaci.length) {
+                        break;
+                    }
+                    niz[i - 1] = Double.parseDouble(podaci[i]);
+                }
+                grad.setTemperature(niz);
+                gradovi.add(grad);
+            }
+        } catch(Exception e) {
+            System.out.println("Problem pri čitanju");
+        } finally {
+            try {
+                ulaz.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return gradovi;
     }
 }
