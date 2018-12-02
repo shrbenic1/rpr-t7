b9c527073dbe8f1878e1a0f3d2349ed697bf04fa
shrbenic1 <shrbenic1@etf.unsa.ba>
2018-12-02 15:08:18 +0100

    Kreiran konstruktor bez parametara, setteri i getteri

--- src/ba/unsa/rpr/tutorijal7/UN.java
+++ src/ba/unsa/rpr/tutorijal7/UN.java
@@ -5,4 +5,15 @@ import java.util.List;
 
 public class UN {
     private List<Drzava> drzave = new ArrayList<>();
+
+    public UN() {
+    }
+
+    public List<Drzava> getDrzave() {
+        return drzave;
+    }
+
+    public void setDrzave(List<Drzava> drzave) {
+        this.drzave = drzave;
+    }
 }
